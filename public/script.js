cc95aa99512765a8115d322bbffa52d88ff5b557
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 11:48:59 -0300

    Incluindo video do host user na tela

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
 const socket = io('/')
 
+const videoGridElement = document.getElementById('video-grid')
+const myVideoElement = document.createElement('video')
+myVideoElement.muted= true
+
 const myPeer = new Peer(undefined, {
   secure: true, 
   host: 'sidinei-peerjs-server.herokuapp.com', 
   port: 443,
 })
+
+configureVideo();
 
 myPeer.on('open', id => { 
   socket.emit('join-room', ROOM_ID, id)
@@ -14,3 +20,20 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
   console.log('User connected:' + userId)
 })
+
+function configureVideo() {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    addVideoStream(myVideoElement, stream)
+  })
+}
+
+function addVideoStream(videoElement, stream) {
+  videoElement.srcObject = stream
+  videoElement.addEventListener('loadedmetadata', () => {
+    videoElement.play()
+  })
+  videoGridElement.append(videoElement)
+}
