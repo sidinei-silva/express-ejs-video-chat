cac6da26c267904c615975318f1c383ac205c668
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 09:07:32 -0300

    Criando rota de room com uuid dinamica

--- server.js
+++ server.js
@@ -8,9 +8,18 @@ const io = require('socket.io')(server,{
   },
 });
 
+const {v4: uuidV4} = require('uuid')
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.get('/', (request, response )=> {
+  response.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (request, response )=> {
+  response.render('room', {roomId: request.params.room})
+})
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {console.log(`Listening in port: ${PORT}`)})
