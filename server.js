ac10c0a67e9bf9c362fc95e183bccad0fdab980b
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 09:05:04 -0300

    Configurando ejs

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ const io = require('socket.io')(server,{
   },
 });
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {console.log(`Listening in port: ${PORT}`)})
