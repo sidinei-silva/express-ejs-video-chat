1741320d7ab03f91fc2f35e3f3b749540132d5b7
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 11:38:51 -0300

    Emitindo o join-room dentro do peer com id peer

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const socket = io('/')
+
 const myPeer = new Peer(undefined, {
   secure: true, 
   host: 'sidinei-peerjs-server.herokuapp.com', 
@@ -6,10 +7,9 @@ const myPeer = new Peer(undefined, {
 })
 
 myPeer.on('open', id => { 
-  console.log(id)
+  socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.emit('join-room', ROOM_ID, 10)
 
 socket.on('user-connected', userId => {
   console.log('User connected:' + userId)
