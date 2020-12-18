e7b2cfdb2abc747a4c65b148f13425b3b8769117
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 12:06:40 -0300

    On disconect do socket e emit user-disconected

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
   })
 })
 
