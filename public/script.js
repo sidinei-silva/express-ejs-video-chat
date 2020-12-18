7e8a7c89e7746d96810cd9ae06bc4d540be3f3ae
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-18 11:35:22 -0300

    Conectando com peer server

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
 const socket = io('/')
+const myPeer = new Peer(undefined, {
+  secure: true, 
+  host: 'sidinei-peerjs-server.herokuapp.com', 
+  port: 443,
+})
+
+myPeer.on('open', id => { 
+  console.log(id)
+})
 
 socket.emit('join-room', ROOM_ID, 10)
 
